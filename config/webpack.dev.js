d18e8c542a3c9096a1dd07d7a65ba6c9f0e2952e
Fernando JS Silva <ferjssilva@gmail.com>
2018-04-19 04:26:09 -0300

    config: add basic webpack configuration

---
+++ config/webpack.dev.js
@@ -0,0 +1,55 @@
+const path = require("path")
+const webpack = require("webpack")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "../src", "index.js"),
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../build"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "build",
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "data/[name].[ext]"
+            }
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(rootDir, 'public', 'index.html'),
+    })
+  ]
+}
