e8e64cd9615189bb42795eaf649817fc90a89a8b
Fernando JS Silva <ferjssilva@gmail.com>
2018-06-08 23:42:21 -0300

    text: uppercase Subnav sample

--- src/layout/sidebarComponent/SidebarComponent.jsx
+++ src/layout/sidebarComponent/SidebarComponent.jsx
@@ -44,13 +44,13 @@ class SidebarComponent extends Component {
           defaultSelectedKeys={['6']}
           mode="inline"
         >
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>subnav 1</span></span>}>
+          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Subnav 1</span></span>}>
             <Menu.Item key="1">Option 1</Menu.Item>
             <Menu.Item key="2">Option 2</Menu.Item>
             <Menu.Item key="3">Option 3</Menu.Item>
             <Menu.Item key="4">Option 4</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>subnav 2</span></span>}>
+          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Subnav 2</span></span>}>
             <Menu.Item key="5">Option 5</Menu.Item>
             <Menu.Item key="6">Option 6</Menu.Item>
             <Menu.Item key="7">Option 7</Menu.Item>
