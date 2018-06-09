e287ddf8d99a69777e10a2cc1aa9771cd7a6f8cd
Fernando JS Silva <ferjssilva@gmail.com>
2018-06-08 23:28:57 -0300

    feature: add mock header to the project

--- src/layout/headerComponent/HeaderComponent.jsx
+++ src/layout/headerComponent/HeaderComponent.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Avatar, Badge, Col, Dropdown, Icon, Input, Layout, Menu, Row } from 'antd';
 
 const { Header } = Layout;
+const { Search } = Input;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a target="blank" href="https://github.com/FerJSsilva/Odds-Reactor">1st menu item</a>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <a target="blank" href="https://github.com/FerJSsilva/Odds-Reactor">2nd menu item</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3">3rd menu item</Menu.Item>
+  </Menu>
+);
 
 const HeaderComponent = () => (
-  <Header>
-    Header
+  <Header
+    className="custom-header"
+  >
+    <Row>
+      <Col span={8}>
+        <Search
+          placeholder="input search text"
+          onSearch={value => alert(value)}
+        />
+      </Col>
+      <Col span={16} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <span>
+          <Badge count={1}><Avatar icon="user" /></Badge>
+        </span>
+        <div className="user-info">
+          <Dropdown overlay={menu} trigger={['click']}>
+            <span style={{ marginLeft: '16px' }}>
+            user name <Icon type="down" />
+            </span>
+          </Dropdown>
+        </div>
+      </Col>
+    </Row>
   </Header>
 );
 
