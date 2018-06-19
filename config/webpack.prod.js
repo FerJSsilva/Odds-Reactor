93aed4cec5d4205497df05e11d95baeea95fba21
Fernando JS Silva <ferjssilva@gmail.com>
2018-06-18 22:42:59 -0300

    config: first webpack build configuration

---
+++ config/webpack.prod.js
@@ -0,0 +1,70 @@
+const path = require("path")
+const webpack = require("webpack")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    path.resolve(__dirname, "../src", "index.js"),
+  ],
+  mode: "production",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../build"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "build",
+    historyApiFallback: true,
+    overlay: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+          },
+          {
+            test: /\.css$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" }
+            ]
+          },
+          {
+            test: /\.(png|jp(e*)g|svg)$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  limit: 10000,
+                  name: "data/[name].[ext]"
+                }
+              }
+            ]
+          },
+          {
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, '../public', 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+}
