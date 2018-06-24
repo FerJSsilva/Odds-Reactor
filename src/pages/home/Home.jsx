6c8cb058f07bdc17eac548e615437a902c2044ce
Fernando JS Silva <ferjssilva@gmail.com>
2018-06-24 19:23:55 -0300

    feature: add basic layout to home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,106 @@
 import React from 'react';
+import { Card, Col, Row } from 'antd';
 
 const Home = () => (
   <div>
-    <h1>Hello World</h1>
+    <Row gutter={16}>
+      <Col span={24}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+    </Row>
+    <Row gutter={16}>
+      <Col span={8}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+      <Col span={8}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+      <Col span={8}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+    </Row>
+    <Row gutter={16}>
+      <Col span={12}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+      <Col span={12}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+    </Row>
+    <Row gutter={16}>
+      <Col span={6}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+      <Col span={6}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+      <Col span={6}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+      <Col span={6}>
+          <Card
+            title="Card title"
+            className="margin-bottom-16"
+            bordered={false}
+          >
+            Card content
+          </Card>
+      </Col>
+    </Row>
   </div>
 );
 
